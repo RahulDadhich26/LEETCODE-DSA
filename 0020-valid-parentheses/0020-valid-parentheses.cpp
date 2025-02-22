f29666d84421b7453a6ed5111ff4f5efe37b7a87
TheGaurdiann <111578507+RahulDadhich26@users.noreply.github.com>
2025-02-22 13:10:26 +0530

    Time: 0 ms (100%), Space: 9 MB (21.3%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> st;
-    unordered_map<char, char> mapping = {
-        {')', '('},
-        {']', '['},
-        {'}', '{'}
-    };
-
-    for (char ch : s) {
-        if (mapping.count(ch)) { // If it's a closing parenthesis
-            if (st.empty() || st.top() != mapping[ch]) {
-                return false; // Unbalanced
+        stack<char>st;
+        unordered_map<char,char>mapping = {
+              {']','['},
+              {')','('},
+              {'}','{'}
+        };
+        for(char ch : s){
+            if(mapping.count(ch)){
+                if(st.empty() || st.top()!= mapping[ch]){
+                    return false;
+                }
+                st.pop();
             }
-            st.pop(); // Pop the matching opening parenthesis
-        } else if (ch == '(' || ch == '[' || ch == '{') { // If it's an opening parenthesis
-            st.push(ch); // Push onto the stack
+            else if(ch == '{' || ch == '[' || ch == '('){
+                st.push(ch);
+            }
         }
-    }
-
-    return st.empty();
+        return st.empty();
     }
 };
