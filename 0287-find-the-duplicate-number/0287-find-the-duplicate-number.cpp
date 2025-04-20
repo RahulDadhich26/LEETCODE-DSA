461a3834dfa3a804fd4f4dac55600de04a0e52fa
TheGaurdiann <111578507+RahulDadhich26@users.noreply.github.com>
2025-04-20 22:33:30 +0530

    Time: 15 ms (43.39%), Space: 65.2 MB (26.08%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
-    int findDuplicate(vector<int>& nums) {
-        int index =0;
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size() - 1;i++){
-             if(nums[i] == nums[i+1]){
-                 index = nums[i];
-                 break;
-             }
+    int findDuplicate(std::vector<int>& nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+
+        // Find the intersection point of the two pointers
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        // Find the entrance of the cycle
+        slow = nums[0];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
         }
-        return index;
+
+        return slow;
     }
 };
