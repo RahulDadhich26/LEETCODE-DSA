0922e5b47fb67dff715e8327f7c7764f9bcefbe2
TheGaurdiann <111578507+RahulDadhich26@users.noreply.github.com>
2025-05-01 23:52:30 +0530

    Time: 0 ms (100%), Space: 8.9 MB (53.19%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -6,7 +6,7 @@ public:
 
         for (int i = 1; i <= rowIndex; ++i) {
             // Traverse from end to start to avoid overwriting values
-            for (int j = res.size() - 1; j >= 1; --j) {
+            for (int j = i - 1; j >= 1; --j) {
                 res[j] = res[j] + res[j - 1];
             }
             res.push_back(1); // Add 1 at the end for the new row
@@ -15,3 +15,17 @@ public:
         return res;
     }
 };
+
+/*
+
+res = [1, 3, 3, 1]
+// inner loop:
+j = 3 → res[3] = res[3] + res[2] = 1 + 3 = 4
+→ res = [1, 3, 3, 4]
+j = 2 → res[2] = res[2] + res[1] = 3 + 3 = 6
+→ res = [1, 3, 6, 4]
+j = 1 → res[1] = res[1] + res[0] = 3 + 1 = 4
+→ res = [1, 4, 6, 4]
+res.push_back(1) → res = [1, 4, 6, 4, 1]
+
+*/
