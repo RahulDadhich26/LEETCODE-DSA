55cdd1e601bf237ed67eed8d1602952bec352f22
TheGaurdiann <111578507+RahulDadhich26@users.noreply.github.com>
2025-05-02 17:54:56 +0530

    Time: 5 ms (84.39%), Space: 22.9 MB (74.21%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -1,33 +1,24 @@
 class Solution {
 public:
-      long long getHoursToEatAll(vector<int>&piles, int bananasPerHour)
-    {
-        long long totalHours = 0;
-        for (int i = 0; i < piles.size(); i++)
-        {
-            int hoursToEatPile = ceil(piles[i] / (double)bananasPerHour);
-            totalHours += hoursToEatPile;
-        }
-        return totalHours;
-    }
-    int minEatingSpeed(vector<int>& piles, int targetHours) {
-        int low = 1, high = *(max_element(piles.begin(), piles.end()));
-        int ans = -1;
-        //================================================================
-        while(low <= high)
-        {
-            int mid = low + (high - low) / 2;
-            long long hoursToEatAll = getHoursToEatAll(piles, mid);
-            
-            if (hoursToEatAll <= targetHours)
-            {
-                ans = mid; //record the answer (this is the best we could record till curr step)
-                high = mid - 1;
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int left = 1;
+        int right = INT_MAX;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            int temp = 0;
+
+            for (int& pile : piles) {
+                temp += pile / mid + (pile % mid != 0);
             }
-            else low = mid + 1;
+
+            if (temp <= h) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
         }
-        //=================================================================
-        return ans;
-    
+
+        return right;
     }
 };
