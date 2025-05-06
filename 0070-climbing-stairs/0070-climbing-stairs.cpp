d95be470012f2b20b7447d69b4a86bd18cd45a4f
TheGaurdiann <111578507+RahulDadhich26@users.noreply.github.com>
2025-05-06 18:24:17 +0530

    Time: 0 ms (100%), Space: 7.7 MB (98.46%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        int dp[n+2];
-        dp[0] = 0;
-        dp[1] = 1;
-        dp[2] = 2;
-        for(int i=3;i<=n;i++){
-            dp[i] = dp[i-1] + dp[i-2];
+        int p = 1;
+        int p2 = 1;
+        for(int i=2;i<=n;i++){
+            int curr = p + p2;
+            p2= p;
+            p = curr;
         }
-        return dp[n];
+        return p;
     }
 };
