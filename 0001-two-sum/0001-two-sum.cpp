8c3d72435ae6ab88a772ba309493ff1bbaaa762f
TheGaurdiann <111578507+RahulDadhich26@users.noreply.github.com>
2025-06-16 10:34:06 +0530

    Time: 4 ms (57.67%), Space: 14.7 MB (58.64%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        for(int i=0;i<nums.size()-1;i++){
-            for(int j = i + 1; j<nums.size();j++){
-                if(nums[i] + nums[j] == target){
-                    return {i , j};
-                }
-            }
+        int n = nums.size() ;
+        vector<int>ans;
+        unordered_map<int,int>mp;
+        for(int i = 0;i<nums.size();i++){
+             if(mp.find(target - nums[i]) == mp.end()) mp[nums[i]] = i;
+             else{
+                ans.push_back(mp[target - nums[i]]);
+                ans.push_back(i);
+                return ans;
+             }
         }
         return {};
     }
