a97dc0e337956b67b8ba600bf3d75da0052cba3c
TheGaurdiann <111578507+RahulDadhich26@users.noreply.github.com>
2025-06-28 00:31:21 +0530

    Time: 103 ms (12.9%), Space: 18.7 MB (37.51%) - LeetHub

---
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& mat, int x) {
+        int n = mat.size();
+      int m = mat[0].size();
+      for(int i=0;i<n;i++){
+          int low = 0 ;
+          int high = m-1;
+          while(low <= high){
+              int mid = low + (high - low)/2;
+              if(mat[i][mid] == x) return true;
+              else if(mat[i][mid] < x) low = mid + 1;
+              else {
+                  high = mid - 1;
+              }
+          }
+      }
+      return false;
+    }
+};
