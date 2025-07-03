8b926b1729b78dd83963bbb38a2abc31224cdbcd
TheGaurdiann <111578507+RahulDadhich26@users.noreply.github.com>
2025-07-03 18:05:20 +0530

    Time: 0 ms (100%), Space: 56.1 MB (97.58%) - LeetHub

---
+++ 0792-binary-search/0792-binary-search.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+     let lo = 0, hi = nums.length - 1;
+    while (lo <= hi) {
+        const mid = Math.floor((lo + hi) / 2);
+        if (nums[mid] === target) return mid;
+        else if (nums[mid] < target) lo = mid + 1;
+        else hi = mid - 1;
+    }
+    return -1;
+};
