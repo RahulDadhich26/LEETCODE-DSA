2d3705256c145c1da044f88b5f8b3e670395ae20
TheGaurdiann <111578507+RahulDadhich26@users.noreply.github.com>
2025-07-11 12:11:06 +0530

    Time: 0 ms (100%), Space: 71.8 MB (16.91%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -5,7 +5,7 @@ public:
         int maxi = INT_MIN ;
         for(int i=0;i<nums.size();i++){
             sum+=nums[i];
-            maxi = max(maxi , sum);;
+            maxi = max(maxi , sum);
             if(sum < 0) sum=0 ;
         }
         return maxi;
