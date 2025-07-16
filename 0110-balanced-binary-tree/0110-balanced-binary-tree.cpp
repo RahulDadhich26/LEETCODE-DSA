b8c5f464b4f0430cf1b7347bb3272ddc988c423c
TheGaurdiann <111578507+RahulDadhich26@users.noreply.github.com>
2025-07-16 05:34:28 +0530

    Time: 0 ms (100%), Space: 23.1 MB (22.96%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -12,17 +12,40 @@
 class Solution {
 public:
     bool isBalanced(TreeNode* root) {
-        if(root == nullptr) return true;
-        if (height(root) == -1)  return false;
-		return true;
-
+        return dfsHeight(root) != -1;
     }
-    int height(TreeNode * root){
-        if(root == NULL) return 0;
-        int leftHeight = height(root->left);
-        int rightHeight = height(root->right);
+    int dfsHeight(TreeNode* root) {
+        // Base case: if the current node is NULL,
+        // return 0 (height of an empty tree)
+        if (root == NULL) return 0;
 
-        if(leftHeight == -1 || rightHeight == -1 || abs(leftHeight - rightHeight) > 1) return -1;
-        return max(leftHeight,rightHeight) + 1;
+        // Recursively calculate the
+        // height of the left subtree
+        int leftHeight = dfsHeight(root->left);
+
+        // If the left subtree is unbalanced,
+        // propagate the unbalance status
+        if (leftHeight == -1) 
+            return -1;
+
+        // Recursively calculate the
+        // height of the right subtree
+        int rightHeight = dfsHeight(root->right);
+
+        // If the right subtree is unbalanced,
+        // propagate the unbalance status
+        if (rightHeight == -1) 
+            return -1;
+
+        // Check if the difference in height between
+        // left and right subtrees is greater than 1
+        // If it's greater, the tree is unbalanced,
+        // return -1 to propagate the unbalance status
+        if (abs(leftHeight - rightHeight) > 1)  
+            return -1;
+
+        // Return the maximum height of left and
+        // right subtrees, adding 1 for the current node
+        return max(leftHeight, rightHeight) + 1;
     }
 };
