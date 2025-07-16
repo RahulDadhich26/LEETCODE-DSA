77dc01c9d686bc3c47dcb5a3ede9e08d033bdac2
TheGaurdiann <111578507+RahulDadhich26@users.noreply.github.com>
2025-07-16 19:19:55 +0530

    Time: 0 ms (100%), Space: 21.4 MB (94.9%) - LeetHub

--- 0112-path-sum/0112-path-sum.cpp
+++ 0112-path-sum/0112-path-sum.cpp
@@ -12,9 +12,12 @@
 class Solution {
 public:
     bool hasPathSum(TreeNode* root, int targetSum) {
-        if(root == nullptr) return false;
-        targetSum = targetSum - root->val;
-        if(root->left == nullptr && root->right == nullptr && targetSum == 0) return true;
-        return hasPathSum(root->left,targetSum) || hasPathSum(root->right,targetSum);
+        if(root == NULL) return false;
+        
+        if(root->left == NULL && root->right == NULL) return (targetSum == root->val);
+        
+        // recursively check kro left or right.
+        int remaining = targetSum - root->val;
+        return hasPathSum(root->left,remaining) || hasPathSum(root->right,remaining);
     }
 };
