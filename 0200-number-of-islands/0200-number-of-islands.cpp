ffc50733b755753524cb20bef455400b38936d4a
Rahul Dadhich <111578507+RahulDadhich26@users.noreply.github.com>
2025-07-23 11:49:33 +0530

    Time: 26 ms (64.76%), Space: 16.1 MB (96.21%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,48 +1,32 @@
 class Solution {
-private:
-    void bfs(int i, int j ,vector<vector<int>>& vis ,vector<vector<char>>& grid ){
-        vis[i][j] = 1;
-        queue<pair<int,int>>q;
-        q.push({i,j});
+public:
+    
+    void dfs(int i , int j , vector<vector<char>>&grid){
         int n = grid.size();
         int m = grid[0].size();
 
-        // 4-directional movement
-        int dx[4] = {-1, 0, 1, 0};
-        int dy[4] = {0, 1, 0, -1};
+        if(i < 0 || i >= n || j < 0 || j >= m || grid[i][j] != '1') return;
 
-        while(!q.empty()){
-            int row = q.front().first;
-            int col = q.front().second;
-            q.pop();
+        grid[i][j] = '0';
+        dfs(i+1,j,grid);
+        dfs(i-1,j,grid);
+        dfs(i,j-1,grid);
+        dfs(i,j+1,grid);
 
-            for(int k = 0; k < 4; k++){
-                int nrow = row + dx[k];
-                int ncol = col + dy[k];
-
-                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && 
-                   grid[nrow][ncol] == '1' && !vis[nrow][ncol]){
-                    vis[nrow][ncol] = 1;
-                    q.push({nrow,ncol});
-                }
-            }
-        }
     }
-
-public:
     int numIslands(vector<vector<char>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<int>>vis(n,vector<int>(m,0));
-        int cnt = 0;
+
+        int count = 0;
         for(int i = 0 ; i < n; i++){
             for(int j = 0 ; j < m; j++){
-                if(!vis[i][j] && grid[i][j] == '1'){
-                    cnt++;
-                    bfs(i,j,vis,grid);
-                }
+                  if(grid[i][j] == '1'){
+                    count++;
+                    dfs(i,j,grid);
+                  }
             }
         }
-        return cnt;
+        return count;
     }
 };
