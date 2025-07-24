507f8a83cbaedb8c7423d17034dc2a0d014d804f
Rahul Dadhich <111578507+RahulDadhich26@users.noreply.github.com>
2025-07-25 05:23:00 +0530

    Time: 8 ms (22.72%), Space: 18.3 MB (55.23%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<int> findOrder(int V, vector<vector<int>>& prerequisites) {
+         vector<int>adj[V];
+        for(auto it : prerequisites){
+            adj[it[1]].push_back(it[0]);
+        }
+        
+        vector<int> indegree(V, 0);
+    
+        // 2. Calculate indegrees
+        for (int i = 0; i < V; ++i) {
+            for (int neighbor : adj[i]) {
+                indegree[neighbor]++;
+            }
+        }
+    
+        // 3. Queue all nodes with indegree 0
+        queue<int> q;
+        for (int i = 0; i < V; ++i) {
+            if (indegree[i] == 0)
+                q.push(i);
+        }
+    
+        // 4. BFS process
+        vector<int> topo;
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            topo.push_back(node);
+    
+            for (int neighbor : adj[node]) {
+                indegree[neighbor]--;
+                if (indegree[neighbor] == 0)
+                    q.push(neighbor);
+            }
+        }
+    
+        if(topo.size() == V) return topo;
+        else return {};
+    }
+};
