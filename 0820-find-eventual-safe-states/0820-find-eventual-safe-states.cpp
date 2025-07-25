08959d1aea469c52ef779f9d874b4e2c05d364db
Rahul Dadhich <111578507+RahulDadhich26@users.noreply.github.com>
2025-07-25 05:53:16 +0530

    Time: 46 ms (56.35%), Space: 66.3 MB (37.54%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
@@ -1,42 +1,34 @@
 class Solution {
 public:
-    // Function to check if a node is eventually safe using DFS
-    bool dfs(int node, vector<vector<int>>& graph, vector<int>& color) {
-        
-        // If node is already visited
-        // If it's marked safe (color == 2), return true
-        // If it's marked visiting (color == 1), that means cycle detected → return false
-        if (color[node] != 0) {
-            if (color[node] == 2) return true;   // already safe
-            else return false;                   // currently in DFS path → cycle
-        }
-        // Mark current node as "visiting" (color = 1)
-        // This helps in cycle detection (we’re in current DFS path)
-        color[node] = 1;
-
-        // Traverse all neighbours of the current node
-        for (int nei : graph[node]) {
-            // If any neighbour leads to a cycle, current node is also unsafe
-            if (!dfs(nei, graph, color)) return false;
-        }
-
-        // All neighbours are safe and no cycle was found,
-        // so this node is safe. Mark it as "safe" (color = 2)
-        color[node] = 2;
-
-        // Return true indicating current node is eventually safe
-        return true;
-    }
-    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        int n = graph.size();
-        vector<int> color(n, 0);
-        vector<int> safe;
-         for (int i = 0; i < n; ++i) {
-            if (dfs(i, graph, color)) {
-                safe.push_back(i);
+    vector<int> eventualSafeNodes(vector<vector<int>>&adj) {
+        int n=adj.size();
+        vector<vector<int>>check(n);
+        vector<int>indegree(n);
+        for(int i=0;i<n;i++){
+            for(auto it:adj[i]){
+                check[it].push_back(i);
+                indegree[i]++;
             }
         }
-
-        return safe;
+        queue<int>q;
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+        vector<int>ans;
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            ans.push_back(node);
+            for(auto it:check[node]){
+                indegree[it]--;
+                if(indegree[it]==0){
+                    q.push(it);
+                }
+            }
+        }
+        sort(ans.begin(),ans.end());
+        return ans;        
     }
 };
