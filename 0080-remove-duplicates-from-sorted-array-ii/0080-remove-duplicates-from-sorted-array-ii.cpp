379f82b85b0c35fe317974e0525601510c5dd444
Rahul Dadhich <111578507+RahulDadhich26@users.noreply.github.com>
2025-08-29 10:39:14 +0530

    Time: 11 ms (34.83%), Space: 19.5 MB (55.83%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
@@ -1,24 +1,15 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        // count
-        // index
-        // i'm using count and index to inplace elements of the array
-        // count them first
-        int occurance = 1; 
-        int index = 1;
-        for(int i=1;i<nums.size();i++){
-            if(nums[i] == nums[i-1]){
-                occurance++;
+        int i = 2;
+        if(nums.size() <= 2) return nums.size();
+
+        for(int j=2;j<nums.size();j++){
+             if(nums[j] != nums[i-2]){
+                nums[i] = nums[j];
+                i++;
             }
-            else {
-                occurance = 1;
-            }
-            if(occurance <= 2){
-            nums[index] = nums[i];
-            index++;
         }
-        }
-        return index;
+        return i;
     }
 };
