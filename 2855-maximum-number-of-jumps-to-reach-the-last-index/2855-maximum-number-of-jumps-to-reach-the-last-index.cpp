77f59a6f1e120bee4efa28eae97ad34c48723584
Rahul Dadhich <111578507+RahulDadhich26@users.noreply.github.com>
2025-09-23 18:01:28 +0530

    Time: 25 ms (31.58%), Space: 68.7 MB (27.99%) - LeetHub

--- 2855-maximum-number-of-jumps-to-reach-the-last-index/2855-maximum-number-of-jumps-to-reach-the-last-index.cpp
+++ 2855-maximum-number-of-jumps-to-reach-the-last-index/2855-maximum-number-of-jumps-to-reach-the-last-index.cpp
@@ -7,7 +7,7 @@ public:
         dp[0] = 0;
         for(int i = 1; i<n;i++){
             for(int j = 0; j<i;j++){
-                if(dp[j] != -1 & abs(nums[i] - nums[j]) <= target){
+                if(dp[j] != -1 && abs(nums[i] - nums[j]) <= target){
                     dp[i] = max(dp[i],dp[j] + 1);
                 }
             }
