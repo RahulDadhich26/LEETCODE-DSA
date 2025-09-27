be6cf92b41fab90f1a7cd0834e531feff10ec0b6
Rahul Dadhich <111578507+RahulDadhich26@users.noreply.github.com>
2025-09-27 23:40:17 +0530

    Time: 0 ms (100%), Space: 21.7 MB (84.32%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        bitset<9> Col[9];
-        bitset<9> Row[9];
-        bitset<9> Block[9];
+        bool Row[9][9];
+        bool Col[9][9];
+        bool Block[9][9];
 
         for(int i=0; i<9; i++){
             for(int j=0; j<9; j++){
                 char c = board[i][j];
                 if (c == '.') continue;
-                int x=(c-'0')%9; 
+                int num = c - '1'; 
+                int boxI = (i/3) * 3 + (j/3);
                 // Convert the character digit to an index (0-8) 0 for 9
 
-                if (Row[i][x]) return 0;
-                Row[i][x] = 1;
-
-                if (Col[j][x]) return 0;
-                Col[j][x] = 1;
+                if(Row[i][num] || Col[j][num] || Block[boxI][num]) return false;
                 
-                int bidx = (i / 3) * 3 + j / 3;
-                if (Block[bidx][x]) return 0;
-                Block[bidx][x] = 1;
+                Row[i][num] = Col[j][num] = Block[boxI][num] = true;
             }
         }
-        return 1;
+        return true;
     }
 };
