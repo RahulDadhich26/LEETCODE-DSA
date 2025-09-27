0c00b767ee527eb4b94ce9b2b57b11a5b0a78189
Rahul Dadhich <111578507+RahulDadhich26@users.noreply.github.com>
2025-09-27 22:12:34 +0530

    Time: 0 ms (100%), Space: 10.1 MB (68.7%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -8,7 +8,7 @@ public:
                 swap(mat[i][j] , mat[j][i]);
             }
         }
-        for(int i = 0 ; i< n ;i++){
+        for(int i = 0 ; i < n ;i++){
             reverse(mat[i].begin(), mat[i].end());
         }
     }
