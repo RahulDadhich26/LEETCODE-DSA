ba7f247583fad18f968b9292cf90a425622754e0
Rahul Dadhich <111578507+RahulDadhich26@users.noreply.github.com>
2025-09-28 19:25:19 +0530

    Time: 1 ms (58.65%), Space: 9.4 MB (56.26%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,28 +1,24 @@
-#include <iostream>
-#include <unordered_map>
-using namespace std;
-
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
         if (s.size() != t.size()) return false;
 
-        unordered_map<char, char> mapS2T;
-        unordered_map<char, char> mapT2S;
+        unordered_map<char, char> m1;
+        unordered_map<char, char> m2;
 
         for (int i = 0; i < s.size(); i++) {
             char c1 = s[i], c2 = t[i];
 
             // Check s → t mapping
-            if (mapS2T.count(c1) && mapS2T[c1] != c2) 
+            if (m1.count(c1) && m1[c1] != c2) 
                 return false;
 
             // Check t → s mapping
-            if (mapT2S.count(c2) && mapT2S[c2] != c1) 
+            if (m2.count(c2) && m2[c2] != c1) 
                 return false;
 
-            mapS2T[c1] = c2;
-            mapT2S[c2] = c1;
+            m1[c1] = c2;
+            m2[c2] = c1;
         }
         return true;
     }
