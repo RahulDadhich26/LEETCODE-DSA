2aaebe082b307f171694fa2728745dd15c7081f2
Rahul Dadhich <111578507+RahulDadhich26@users.noreply.github.com>
2025-10-07 00:34:24 +0530

    Time: 0 ms (100%), Space: 22.1 MB (18.2%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -1,38 +1,21 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
+    long long prev = LLONG_MIN;
+
+    bool inorder(TreeNode* root) {
+        
+        if(!root) return true;
+
+        if(!inorder(root->left)) return false;
+
+        if(root->val <= prev) return false;
+
+        prev = root->val;
+        
+        return inorder(root->right);
+    }
+
     bool isValidBST(TreeNode* root) {
-        stack<TreeNode*>st;
-        TreeNode* curr = root;
-        long long prev = LLONG_MIN;
-
-        while(curr != nullptr || !st.empty()){
-            // traverse to the leftmost node
-            // me pehele left me juanga chota element dekhne
-            while(curr != nullptr){
-                st.push(curr);
-                curr = curr->left;
-            } 
-            // current node is now the leftmost 
-            curr = st.top();
-            st.pop();
-            
-            if(curr->val <= prev) return false;
-
-            prev = curr->val;
-            curr = curr->right;
-
-        }
-        return true;
+        return inorder(root);
     }
 };
