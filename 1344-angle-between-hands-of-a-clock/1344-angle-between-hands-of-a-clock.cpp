e7f4df459cc2e845a6a7cdb99df8745196b925e3
Rahul Dadhich <111578507+RahulDadhich26@users.noreply.github.com>
2025-11-30 19:49:29 +0530

    Time: 0 ms (100%), Space: 8.2 MB (43.13%) - LeetHub

---
+++ 1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    double angleClock(int hour, int minutes) {
+        double minAngle = minutes * 6.0;
+        double hourAngle = (hour % 12) * 30.0 + minutes * 0.5;
+        
+        double diff = fabs(hourAngle - minAngle);
+        return min(diff, 360.0 - diff);
+    }
+};
